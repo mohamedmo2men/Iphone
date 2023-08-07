9a25ddbda04e741fcd0b2c98e83311e35d19adf8
Mohamed Moamen <80352023+mohamedmo2men@users.noreply.github.com>
2023-08-07 17:18:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let iphone = document.querySelector('.iphone');
+let container = document.querySelector('.container');
+
+function phones(phone) {
+    iphone.src =  phone;
+    
+}
+function colors(color) {
+    container.style.background = color;
+    
+}
